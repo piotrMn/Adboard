a5d8900f5e1c5ba9718fbdf7ec4deac46eeb67c1
piotr <piotr.muehleisen@gmail.com>
2018-07-09 10:57:11 +0200

    bug in script.js fixed

--- src/main/webapp/WEB-INF/js/script.js
+++ src/main/webapp/WEB-INF/js/script.js
@@ -60,7 +60,7 @@ $(function() {
 			var content = comment.content;
 			var d = new Date();
 			var dateFormatted = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
-			console.log(date_format_str);
+			console.log(dateFormatted);
 
 			var newDiv = $("<div class='comment-item'>" + fullname + " (" + dateFormatted +  ")<br>" + content + "</div>");
 			commentBox.prepend(newDiv);
